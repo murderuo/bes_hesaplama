ab69094eb0dec3dad14e93270464d7366455462e
UgurOK <murderuo@users.noreply.github.com>
2024-05-12 01:30:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+baslangic_tutari=int(input('bes primini giriniz:'))
+yillik_artis_miktari=int(input('yıllık artış miktarını giriniz:'))
+yillik_getiri_miktari=int(input('yıllık getiri miktari:'))
+degerlendirilecek_sure=int(input('süreyi giriniz:'))
+
+aylik_getiri=yillik_getiri_miktari/12
+birikim=0
+birikim_getirisi=0
+toplam_birikim=0
+for ay in range(1,degerlendirilecek_sure):
+    birikim+=baslangic_tutari
+    birikim_getirisi=birikim*aylik_getiri/100
+    toplam_birikim+=birikim_getirisi
+    if ay%12==0:
+        aylik_getiri+=aylik_getiri*yillik_artis_miktari/100
+        baslangic_tutari+=baslangic_tutari*yillik_artis_miktari/100
+    print(f'ödenen toplam pirim:{birikim:.2f}\t\t\t\t\t\t prim getirisi:{birikim_getirisi:.2f}\t\t\t\t\t\tprim miktarı:{baslangic_tutari:.2f} \t aylık getiri oranı:{aylik_getiri}')
+    
+print(f'{toplam_birikim:.2f}')
+
+
+
+    
+    
+    
